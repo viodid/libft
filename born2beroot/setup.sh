e16c2bcceeec3e070772a8c2f17b83599f730bfe
viodid <david.zwsse@simplelogin.co>
2023-03-24 22:24:21 +0100

    password policies, group and user creation

--- born2beroot/setup.sh
+++ born2beroot/setup.sh
@@ -7,6 +7,7 @@
 # https://stackoverflow.com/a/28938235
 # expample
 # echo -e "I ${Red}love${Yellow} Stack Overflow"
+
 Black='\033[0;30m'        # Black
 Red='\033[0;31m'          # Red
 Green='\033[0;32m'        # Green
@@ -25,11 +26,11 @@ echo -en "${Purple}
 ██████╦╝╚█████╔╝██║░░██║██║░╚███║███████╗██████╦╝███████╗██║░░██║╚█████╔╝╚█████╔╝░░░██║░░░
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░░╚════╝░░░░╚═╝░░░
 "
+
 # Disclaimer
 echo -en "${Green}\nBe aware that the script must be executed with root privileges, otherwise the script will fail.\nIf you don't know how to login as a root, try: ${Yellow}sudo su -\n"
 
 # Let the user write which linux distro is the script running on
-
 # Linux distro election; 1 for Debian, 2 for CentOs
 while true; do
 	echo -e "\n${Cyan}What distro have you chose?\n${White}1. Debian\n2. CentOs"
@@ -46,21 +47,29 @@ while true; do
 done
 
 
-echo -e "Setting up enviroment\n${Cyan}Please enter you username (login)...${White}"
+echo -e "Setting up enviroment\n${Cyan}Please enter you username (login):${White}"
 read username
 
 # Set the hostname
+echo -n "Changing hostname..."
 echo "${username}42" > /etc/hostname
-echo -e "127.0.0.1\t"${username}42"" > /etc/hosts
+echo -e "127.0.0.1\t"${username}42"\n::1\t\t"${username}42"" > /etc/hosts
+
+echo -e "Changing password policies..."
+# Set password expiration policy
+chage -M 30 -m 2 -W 7
+
+# Change password policy in /etc/login.defs
+sed -i '/^PASS_MAX_DAYS/ c\PASS_MAX_DAYS   30' /etc/login.defs
+sed -i '/^PASS_MIN_DAYS/ c\PASS_MIN_DAYS   2' /etc/login.defs
+sed -i '/^PASS_WARN_AGE/ c\PASS_WARN_AGE   7' /etc/login.defs
+
+# Change password policy in /etc/security/pwquality.conf
+echo "difok = 7\nminlen = 10\ndcredit = -1\nucredit = -1\nlcredit = -1\nmaxrepeat = 3\nusercheck = 1\nenforcing = 1\nretry = 3\nenforce_for_root" >> /etc/security/pwquality.conf
+
+echo -en "${Green}Done!${White}"
 
 
-# Update the hosts file with the new hostname
-# sed -i "s/127.0.0.1*/127.0.0.1\t"${username}42"/" /etc/hosts
-
-
-
-#echo -e "${Cyan}Write your username (login):${White}"
-#read USERNAME
 
 #sudo useradd -m -s $SHELL -g wheel -c "User created with bor2beroot script" $USERNAME
 
