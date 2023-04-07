82fb6f7c8edcf1bca68ac3a7bc637bf2e45e617b
viodid <david.yunta.aller@gmail.com>
2023-04-07 13:17:49 +0200

    centos check

--- born2beroot/setup.sh
+++ born2beroot/setup.sh
@@ -92,10 +92,10 @@ distro=$(cat /etc/os-release | grep -i ^id= | cut -d '=' -f 2)
 
 echo $distro
 # Linux distro election; 1 for Debian, 2 for CentOs
-if [[ $distro == debian ]]; then
+if [[ "$distro" == debian ]]; then
 	echo -e "${Green}You are running Debian!${White}"
 	distro=1
-elif [[ $distro == "centos" ]]; then 
+elif [[ "$distro" == "centos" ]]; then 
 	echo -e "${Green}You are running CentOs!\nNote that this script only supports CentOS Stream 8 or higher.${White}"
 	distro=2
 else
