dabc84bafc836782d9380a965853f937eb01335a
viodid <david.yunta.aller@gmail.com>
2023-04-11 22:13:49 +0200

    add my_monitoring

--- born2beroot/my_monitoring.sh
+++ born2beroot/my_monitoring.sh
@@ -1,6 +1,8 @@
+ram_usage=$(free -m | awk '/Mem:/ { print $3}')
+echo ram_usage
 wall "
   #Architecture: $(uname -a)
   #CPU physical: $(egrep "^processor" /proc/cpuinfo | wc -l)
   #vCPU: $(egrep "^processor" /proc/cpuinfo | wc -l)
-  #Memory Usage: $(free -m | awk '/Mem:/ { print $2}')
+  #Memory Usage: $(free -m | awk '/Mem:/ { print $3}')MiB/$(free -m | awk '/Mem:/ { print $2}')MiB
 "
