16026e7d6cdb8b1890cfa46edbe3ce4c256d87f7
viodid <david.yunta.aller@gmail.com>
2023-04-13 14:24:03 -0400

    my_monitoring

--- born2beroot/my_monitoring.sh
+++ born2beroot/my_monitoring.sh
@@ -1,9 +1,10 @@
 ram_usage=$(free -m | awk '/Mem:/ { print $3}')
 ram_total=$(free -m | awk '/Mem:/ { print $2}')
+percentage=$(echo "scale=2; $ram_usage / $ram_total" | bc)
+echo $percentage
 wall "
   #Architecture: $(uname -a)
   #CPU physical: $(grep "^processor" /proc/cpuinfo | wc -l)
-  #vCPU: $(egrep "^processor" /proc/cpuinfo | wc -l)
-  #Memory Usage: $(ram_usage)MiB/$(ram_total)MiB ($("scale=2; $ram_usage / $ram_total" | bc))
-  #
+  #vCPU: $(grep "^processor" /proc/cpuinfo | wc -l)
+  #Memory Usage: ${ram_usage}MiB/${ram_total}MiB ${percentage}
 "
