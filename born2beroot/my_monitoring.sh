05a2953f470cc94a8ebfa15070d2d26cf8e7f73e
David Yunta <david.yunta.aller@gmail.com>
2023-04-13 20:02:22 +0200

    Update my_monitoring.sh

--- born2beroot/my_monitoring.sh
+++ born2beroot/my_monitoring.sh
@@ -4,6 +4,6 @@ wall "
   #Architecture: $(uname -a)
   #CPU physical: $(grep "^processor" /proc/cpuinfo | wc -l)
   #vCPU: $(egrep "^processor" /proc/cpuinfo | wc -l)
-  #Memory Usage: $(ram_usage)MiB/$(ram_total)MiB ($"scale=2; $ram_usage / $ram_total" | bc)
+  #Memory Usage: $(ram_usage)MiB/$(ram_total)MiB ($("scale=2; $ram_usage / $ram_total" | bc))
   #
 "
