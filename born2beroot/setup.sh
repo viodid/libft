e2018dc081ae0d05eea3c0c785a0a572b19014d0
viodid <david.yunta.aller@gmail.com>
2023-04-24 20:07:36 +0200

    change monitoring location

--- born2beroot/setup.sh
+++ born2beroot/setup.sh
@@ -209,7 +209,7 @@ sleep 1 & wait
 echo "Adding monitoring.sh to crontab..."
 cp ./monitoring.sh /home/$username/
 chmod 775 /home/$username/monitoring.sh
-echo -e "*/10 * * * * root /root/monitoring.sh" > /etc/cron.d/monitoring
+echo -e "*/10 * * * * /home/${username}/" > /etc/cron.d/monitoring
 systemctl restart crond > /dev/null
 sleep 1 & wait
 
