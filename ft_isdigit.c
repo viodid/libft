0cec7bc128fbd505fba4ffd11b9ae321fc8fe4c0
David Yunta <david.yunta.aller@gmail.com>
2023-11-29 21:03:36 +0100

    add description ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -5,10 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/08 00:04:15 by dyunta            #+#    #+#             */
-/*   Updated: 2023/02/08 00:04:15 by dyunta           ###   ########.fr       */
+/*   Created: 2023/11/27 20:35:49 by dyunta            #+#    #+#             */
+/*   Updated: 2023/11/29 21:02:39 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+ * Returns zero if the character tests false and return non-zero if the
+ * character tests true.
+ */
 
 int	ft_isdigit(int c)
 {
