86e51d489a666b60aa322d26816a42e03f6c53bb
David Yunta <david.yunta.aller@gmail.com>
2023-11-29 21:03:56 +0100

    add description ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -5,10 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dyunta <dyunta@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/08 00:04:15 by dyunta            #+#    #+#             */
-/*   Updated: 2023/02/08 00:04:15 by dyunta           ###   ########.fr       */
+/*   Created: 2023/11/27 20:35:49 by dyunta            #+#    #+#             */
+/*   Updated: 2023/11/29 21:03:14 by dyunta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+ *  Tests for any printing character, including space (' ').  The value of the
+ *  argument must be representable as an unsigned char or the value of EOF.
+ *  Returns zero if the character tests false and returns non-zero if the
+ *  character tests true.
+ */
 
 int	ft_isprint(int c)
 {
